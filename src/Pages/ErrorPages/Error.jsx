2ec0cb00f4b4990bf4fbd8ee8b4b6802c3f698aa
Sahajewel <jewelsaha072@gmail.com>
2025-01-14 21:38:25 +0900

    banner completed

--- src/Pages/ErrorPages/Error.jsx
+++ src/Pages/ErrorPages/Error.jsx
@@ -6,7 +6,7 @@ export default function Error() {
   return (
     <div className="flex flex-col min-h-screen justify-center w-10/12 items-center">
       <h1 className="text-4xl font-bold mb-2">Oops!</h1>
-      <p className="text-4xl font-bold mb-2">404 Page</p>
+      <p className="text-4xl font-bold mb-2">404 Page not found</p>
       <p className="text-4xl font-bold mb-2">{error.statusText || error.message}</p>
      <Link to="/"> <button className="btn mt-2">Back to Home</button> </Link>
     </div>
