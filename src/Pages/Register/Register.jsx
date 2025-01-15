7bcc53d009dae1ef61e367111f3b1ae91fcd6e6b
Sahajewel <jewelsaha072@gmail.com>
2025-01-15 21:41:17 +0900

    user create in database completed

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -5,14 +5,24 @@ import { AuthContext } from "../../Provider/AuthProvider";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Swal from "sweetalert2";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
 export default function Register() {
     const { createUser, updateUserProfile, google } = useContext(AuthContext);
     const navigate = useNavigate();
-    const location = useLocation()
+    const location = useLocation();
+    const axiosPublic = useAxiosPublic()
     const handleGoogle = () => {
         google()
             .then((result) => {
                 console.log(result.user)
+                const userInfo = {
+                    name: result.user?.displayName,
+                    email: result.user?.email
+                }
+                axiosPublic.post("/users", userInfo)
+                .then((res)=>{
+                    console.log(res.data)
+                })
                 Swal.fire({
                     position: "top-end",
                     icon: "success",
@@ -35,19 +45,37 @@ export default function Register() {
             .then((result) => {
                 console.log(result.user)
                 updateUserProfile(data.name, data.photo)
-                Swal.fire({
-                    position: "top-end",
-                    icon: "success",
-                    title: "Succefully signup",
-                    showConfirmButton: false,
-                    timer: 1500
-                });
-                navigate(location?.state ? location.state : "/")
-                    .then((result) => {
-                        console.log(result.user)
-
-                    })
-                reset()
+                .then((result)=>{
+                    console.log(result)
+                    const userProfile ={
+                        name: data.name,
+                        email: data.email,
+                     }
+                     axiosPublic.post("/users", userProfile)
+                     .then((res)=>{
+                        console.log(res.data)
+                        reset()
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Succefully signup",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                       
+                        
+                     })
+                     navigate(location?.state ? location.state : "/")
+                     
+                })
+               .catch((error)=>{
+                console.log(error)
+               })
+               
+                    
+                   
+                    
+                
             })
     }
     return (
