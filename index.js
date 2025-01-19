c17334412e155e27e1202106ebc8aaa5308079a6
Sahajewel <jewelsaha072@gmail.com>
2025-01-19 14:41:45 +0900

    jwt api created

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const cors = require("cors");
-// require('dotenv').config();
+
+const jwt = require("jsonwebtoken")
+require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 app.use(cors({
-  origin: ["http://localhost:5173"]
+  origin: ["http://localhost:5173", "https://clever-entremet-18d091.netlify.app"]
 }));
 app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://fitnessCenter:W9mEPP8mxzXO12y9@cluster0.r7awt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.BD_PASS}@cluster0.r7awt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -29,7 +31,41 @@ async function run() {
    const paymentCollection = client.db("fitnessDB").collection("payment");
    const becomeATrainerCollection = client.db("fitnessDB").collection("becomeATrainer");
    const addAClassCollection = client.db("fitnessDB").collection("addAClass");
-  
+
+
+  //  jwt collection
+  app.post("/jwt", (req, res)=>{
+    const user = req.body;
+    const token = jwt.sign(user,process.env.JWT_TOKEN_ACCESS, {expiresIn : "365d"})
+    res.send({token})
+  })
+
+  //  verify token
+  const verifyToken = (req, res, next)=>{
+     if(!req.headers.authorization){
+       return res.status(401).send({message: "Unauthorized Access"})
+     }
+     const token = req.headers.authorization.split(" ")[1]
+     jwt.verify(token, process.env.JWT_TOKEN_ACCESS, (err, decoded)=>{
+      if(err){
+        return res.status(401).send({message: "Unauthorized Acces"})
+      }
+      req.decoded = decoded
+      next()
+     })
+  }
+
+  // verify admin
+  const verifyAdmin = async(req, res, next)=>{
+    const email = req?.decoded?.email;
+    const query = {email: email};
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === "admin";
+    if(!isAdmin){
+      return res.status(403).send({message: "forbidden access"})
+    }
+    next()
+  }
   //  users collection
     app.post("/users", async(req, res)=>{
       const user = req.body;
@@ -42,6 +78,46 @@ async function run() {
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
+    app.get("/users/admin/:email", verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req?.decoded?.email){
+        return res.status(403).send({message: "Forbidden Access"})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query)
+      let admin = false;
+      if(user){
+        admin = user?.role === "admin"
+      }
+      res.send(admin)
+    })
+    app.get("/users/trainer/:email",verifyToken, async(req, res)=>{
+      const email = req.params.email;
+      if(email !== req?.decoded?.email){
+        return res.status(403).send({message: "Forbidden Access"})
+      }
+      const query ={email : email};
+      const user = await userCollection.findOne(query);
+      let trainer = false;
+      if(user){
+        trainer = user?.role === "trainer"
+      }
+      res.send(trainer)
+
+    })
+    app.patch("/users", async(req, res)=>{
+      const {email, name, image} = req.body;
+        const query = {email};
+        const updteDoc = {
+             $set: {
+              name,
+              image
+             }
+        }
+        const result = await userCollection.updateOne(query, updteDoc)
+        res.send(result)
+
+    })
 
     // newsletter collection
 
@@ -50,7 +126,7 @@ async function run() {
       const result = await newsletterCollection.insertOne(news)
       res.send(result)
     })
-    app.get("/newsletter", async(req, res)=>{
+    app.get("/newsletter",verifyToken, verifyAdmin, async(req, res)=>{
       const result = await newsletterCollection.find().toArray()
       res.send(result)
     })
@@ -96,7 +172,7 @@ async function run() {
 
     // add a class
 
-    app.post("/add-a-class", async(req, res)=>{
+    app.post("/add-a-class",verifyToken, verifyAdmin, async(req, res)=>{
       const AddClass = req.body;
       const result = await addAClassCollection.insertOne(AddClass);
       res.send(result)
